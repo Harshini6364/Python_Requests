2cb729046c30a73bd183b3061ebf0c055975c1fd
Harshini <24b01a1223@svecw.edu.in>
2025-10-04 18:16:48 +0530

    Added some common status codes

--- tutorial.py
+++ tutorial.py
@@ -49,4 +49,29 @@ headers={
     'Authorization': 'Bearer 1234567'
 }
 bearer_auth_response=requests.get('https://httpbin.org/bearer')
-print(bearer_auth_response)
+#print(bearer_auth_response)
+
+#6. Some Common Status Codes
+# # 200 - OK
+ok = requests.get('https://httpbin.org/status/200')
+#print(ok)
+
+# # 400 - Bad Request
+bad_request = requests.get('https://httpbin.org/status/400')
+print(bad_request)
+
+# # 401 - Unauthorized
+# unauthorized = requests.get('https://httpbin.org/basic-auth/testuser/password')
+# print(unauthorized)
+
+# # 403 - Forbidden
+# forbidden = requests.get('https://httpbin.org/status/403')
+# print(forbidden)
+
+# # 404 - Not Found
+# not_found = requests.get('https://jsonplaceholder.typicode.com/todo')
+# print(not_found)
+
+# # 500 - Server Errors
+# server_error = requests.get('https://httpbin.org/status/500')
+# print(server_error)
