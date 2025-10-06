7abcea9e91b60e0b11672316f71315d9310581a8
Harshini <24b01a1223@svecw.edu.in>
2025-10-06 18:59:03 +0530

    Added some more common status codes

--- tutorial.py
+++ tutorial.py
@@ -58,20 +58,8 @@ ok = requests.get('https://httpbin.org/status/200')
 
 # # 400 - Bad Request
 bad_request = requests.get('https://httpbin.org/status/400')
-print(bad_request)
+#print(bad_request)
 
 # # 401 - Unauthorized
-# unauthorized = requests.get('https://httpbin.org/basic-auth/testuser/password')
+unauthorized = requests.get('https://httpbin.org/basic-auth/testuser/password')
 # print(unauthorized)
-
-# # 403 - Forbidden
-# forbidden = requests.get('https://httpbin.org/status/403')
-# print(forbidden)
-
-# # 404 - Not Found
-# not_found = requests.get('https://jsonplaceholder.typicode.com/todo')
-# print(not_found)
-
-# # 500 - Server Errors
-# server_error = requests.get('https://httpbin.org/status/500')
-# print(server_error)
